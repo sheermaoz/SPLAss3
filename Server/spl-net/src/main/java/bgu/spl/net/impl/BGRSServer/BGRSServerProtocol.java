941176d35b77cfaeae583066b301932ee3acb464
Sheer Maoz <sheermaoz@gmail.com>
2020-12-31 00:03:06 +0200

    Started Implamentation

--- Server/spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/BGRSServerProtocol.java
+++ Server/spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/BGRSServerProtocol.java
@@ -4,9 +4,10 @@ import bgu.spl.net.Database;
 import bgu.spl.net.api.MessagingProtocol;
 import bgu.spl.net.srv.Messages.*;
 import bgu.spl.net.srv.Messages.Error;
+import bgu.spl.net.srv.Type;
 import bgu.spl.net.srv.User;
 
-public class BGRSServerProtocol implements MessagingProtocol<Message> {
+public class BGRSServerProtocol implements MessagingProtocol<Message<?>> {
 
     private User user;
     private Database database;
@@ -16,15 +17,26 @@ public class BGRSServerProtocol implements MessagingProtocol<Message> {
         database = Database.getInstance();
     }
 
-    public Message process(Message msg)
+    public Message<Short> process(Message<?> msg)
     {
         boolean success = true;
-        if (msg.getClass().equals(Login.class))
+        if (user == null)
         {
-            user = database.login(((Login)msg).getName(), ((Login)msg).getPassword());
-            if (user == null)
+            if (msg.getOpcode() == 3)
             {
-                success = false;
+                user = database.login((String)msg.getArgs()[0], (String)msg.getArgs()[1]);
+                if (user == null)
+                {
+                    success = false;
+                }
+            }
+            else if (msg.getOpcode() == 1)
+            {
+                success = database.register((String)msg.getArgs()[0], Type.Admin, (String)msg.getArgs()[1]);
+            }
+            else if (msg.getOpcode() == 2)
+            {
+                success = database.register((String)msg.getArgs()[0], Type.User, (String)msg.getArgs()[1]);
             }
         }
 
