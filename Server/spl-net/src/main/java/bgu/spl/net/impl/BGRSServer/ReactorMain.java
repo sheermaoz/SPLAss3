fbb1a2c2cab0012d965ac82249ae190af4529c5b
Sheer Maoz <sheermaoz@gmail.com>
2021-01-03 18:42:56 +0200

    Created mains

---
+++ Server/spl-net/src/main/java/bgu/spl/net/impl/BGRSServer/ReactorMain.java
@@ -0,0 +1,21 @@
+package bgu.spl.net.impl.BGRSServer;
+
+import bgu.spl.net.srv.Server;
+
+public class ReactorMain {
+    public static void main(String[] args) {
+        if (args.length != 2)
+        {
+            System.out.println("USAGE: ReactorMain <numOfThreads> <port>");
+            return;
+        }
+        Server.reactor(
+            Integer.valueOf(args[0]), 
+            Integer.valueOf(args[1]), 
+            ()->{return new BGRSServerProtocol();}, 
+            ()->{return new MsgEncDec();}
+            ).serve();
+    
+    }
+    
+}
